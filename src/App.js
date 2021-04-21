6d2eb3d5c2464ea01d5d7d5dda80b63abb82d790
Kuldeep Yadav <ikuldeepyadavky@gmail.com>
2021-04-21 21:36:43 +0530

    Building the chat feed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { ChatEngine } from 'react-chat-engine'
+import ChatFeed from './components/ChatFeed'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<ChatEngine
+			height='100vh'
+			projectID='da01f184-3502-4319-b3f3-1cd6269ee117'
+			userName='kuldeepyadavky'
+			userSecret='123456'
+			renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+		/>
+	)
 }
 
-export default App;
+export default App
